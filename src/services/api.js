ae907a1ab1ef47a22c0884e68fca273ac8015c41
Rahithyasai <rahithyasaikumari_chapalamadugu@srmap.edu.in>
2026-07-28 22:06:29 +0530

    Update API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://study-assistance.onrender.com",
 });
 
 export const generateStudyContent = async (data) => {
